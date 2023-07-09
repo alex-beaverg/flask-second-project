8e2cfedffcc00b8dfde36d3d7ea9e71425a4762b
Alexey Bobrikov <a.beaverg@gmail.com>
2023-07-09 20:55:38 +0300

    upload third step

--- api/app.py
+++ api/app.py
@@ -1,6 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///info.db'
+db = SQLAlchemy(app)
+
+
+class Information(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    client = db.Column(db.String(50), nullable=False)
+    connection = db.Column(db.String(50), nullable=False)
+    integration = db.Column(db.String(50), nullable=False)
+    payment_method = db.Column(db.String(50), nullable=False)
+    payers = db.Column(db.String(50), nullable=False)
+    payment_system = db.Column(db.String(50), nullable=False)
+    bank = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return '<Information %r>' % self.id
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -15,8 +32,29 @@ def index():
             'payment_system': request.form['payment_system'],
             'bank': request.form['bank']
         }
-        return render_template('index.html', info=info)
-    return render_template('index.html')
+        information = Information(
+            client=info['client'],
+            connection=info['connection'],
+            integration=info['integration'],
+            payment_method=info['payment_method'],
+            payers=info['payers'],
+            payment_system=info['payment_system'],
+            bank=info['bank']
+        )
+        try:
+            db.session.add(information)
+            db.session.commit()
+            return redirect('/orders')
+        except:
+            return '[ERROR]: Information has not been added!'
+    else:
+        return render_template('index.html')
+
+
+@app.route('/orders')
+def orders():
+    all_orders = Information.query.order_by(Information.id).all()
+    return render_template('orders.html', all_orders=all_orders)
 
 
 if __name__ == '__main__':
