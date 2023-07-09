86f6103a1cf680e453ef5f5de29b6b1a4c25f026
Alexey Bobrikov <a.beaverg@gmail.com>
2023-07-09 20:12:24 +0300

    upload second step

--- api/app.py
+++ api/app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
+    if request.method == 'POST':
+        info = {
+            'client': request.form['client'],
+            'connection': request.form['connection'],
+            'integration': request.form['integration'],
+            'payment_method': request.form['payment_method'],
+            'payers': request.form['payers'],
+            'payment_system': request.form['payment_system'],
+            'bank': request.form['bank']
+        }
+        return render_template('index.html', info=info)
     return render_template('index.html')
 
 
